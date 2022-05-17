e263b226ee635c0ae591daac818a9c57158c8509
Doc077 <dom.rajkowski@gmail.com>
2022-05-17 20:57:00 +0200

    Fix type issues

--- src/database/db.class.ts
+++ src/database/db.class.ts
@@ -1,4 +1,5 @@
-import { createConnection, Connection, MysqlError } from 'mysql'
+import { createConnection, Connection, MysqlError, Query as MysqlQuery } from 'mysql'
+import { Config } from '../config/config.class'
 import { Exception } from '../handler/exception.class'
 import { Query } from './query.class'
 
@@ -7,18 +8,20 @@ export abstract class DB {
 
   public static establishConnection(): void {
     this.connection = createConnection({
-      host: '',
+      host: Config.database.host,
+      user: Config.database.user,
+      password: Config.database.password,
     })
   }
 
-  public static async raw(query: string): Promise<any[]> {
+  public static raw(query: string): Promise<any[]> | MysqlQuery | undefined {
     this.connection?.connect((error: MysqlError) => {
       if (error) {
         throw new Exception('Database connection failed')
       }
     })
 
-    const result = await this.connection?.query(query)
+    const result = this.connection?.query(query)
 
     return result
   }

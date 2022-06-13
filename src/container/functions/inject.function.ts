1311c8518854ac055944cb43e314669c829b490d
Doc077 <dom.rajkowski@gmail.com>
2022-06-13 20:29:16 +0200

    Update type

--- src/container/functions/inject.function.ts
+++ src/container/functions/inject.function.ts
@@ -1,5 +1,6 @@
+import { Constructor } from '../interfaces/constructor.interface'
 import { Injector } from '../injector.class'
 
-export const inject = (service: any): any => {
+export const inject = (service: Constructor<any>): any => {
   return Injector.resolve<any>(service)
 }

6186d2a20f7a121de38c164affcc94b2a93878dc
Doc077 <dom.rajkowski@gmail.com>
2022-06-15 20:15:10 +0200

    Handle channel authorization

--- src/socket/broadcaster.class.ts
+++ src/socket/broadcaster.class.ts
@@ -1,16 +1,17 @@
 import { Server as BroadcastServer, Socket } from 'socket.io'
 import { Server } from 'http'
+import { Authorize } from '../contracts/authorize.interface'
 import { Injector } from '../container/injector.class'
 import { Logger } from '../console/logger.class'
 
 export class Broadcaster {
   private static broadcastServer: null | BroadcastServer = null
 
-  private static channels: any[] = []
+  private static channels: Authorize[] = []
 
   public static emit(event: string, channelName: string, ...data: any[]): void {
-    this.channels.forEach((channel: any) => {
-      if (channel.nameRegex.test(channelName)) {
+    this.channels.forEach((channel: Authorize & any) => {
+      if (channel.nameRegex.test(channelName) && channel.userAuthorized()) {
         this.broadcastServer?.emit(`${channelName}/${event}`, ...data)
 
         Logger.success(`Broadcasted event: ${channelName}/${event}`, 'socket')
